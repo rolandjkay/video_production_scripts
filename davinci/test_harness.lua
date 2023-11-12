45914edd02df3d8d700c7cf3901b180483089683
rolandjkay <roland.j.kay@googlemail.com>
2023-11-12 19:55:20 +0000

    Squash
    Simpler implementation of the squash output.

--- davinci/test_harness.lua
+++ davinci/test_harness.lua
@@ -1,3 +1,11 @@
+--[[
+ Test harness for fuses. Run with the following command:
+
+ dofile("C:\\Users\\rolan\\Dev\\video_production_scripts\\davinci\\test_harness.lua")
+
+]]
+
+
 -- Dummy function
 function FuRegisterClass(arg1, arg2, arg3)
 end
@@ -6,13 +14,7 @@ dofile("C:\\Users\\rolan\\Dev\\video_production_scripts\\davinci\\bounce_modifie
 
 for frame_num = 0.0, 100.0, 0.2
 do
-	data = bounce_get_height_at_t(20, 10.0, nil, 0.6, 10.0, 25, 0, 1.0, 0.0, frame_num)
-	print(frame_num, ",", data[1], ",", data[2], ",", data[3])
-end
-
-for frame_num = 0.0, 100.0, 0.2
-do
-	data = bounce_get_height_at_t(20, 10.0, 50.0/20.0, 0.6, 10.0, 25, 0, 1.0, 0.0, frame_num)
+	data = bounce_get_height_at_t(20, 10.0, nil, 0.6, 10.0, 25, 0, 1.0, 0.0, false, 8.0, 0.3, frame_num)
 	print(frame_num, ",", data[1], ",", data[2], ",", data[3])
 end
 
